b481aac883e7225e773ba6cf099eb26324fd50f2
YoungRj <yrjssm@163.com>
2020-08-11 22:29:37 +0800

    学习智慧北京项目

--- app/src/main/java/com/yrj/zhbj/base/impl/NewsCenterPager.java
+++ app/src/main/java/com/yrj/zhbj/base/impl/NewsCenterPager.java
@@ -4,7 +4,14 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.view.Gravity;
 import android.widget.TextView;
+import android.widget.Toast;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 import com.yrj.zhbj.base.BasePager;
+import com.yrj.zhbj.global.GlobalConstants;
 
 public class NewsCenterPager extends BasePager {
 
@@ -14,6 +21,7 @@ public class NewsCenterPager extends BasePager {
 
     @Override
     public void initData() {
+        System.out.println("新闻中心初始化了。。。。");
         //给空的帧布局动态添加布局
         TextView view = new TextView(mActivity);
         view.setText("新闻中心");
@@ -24,5 +32,27 @@ public class NewsCenterPager extends BasePager {
         flContainer.addView(view);//给帧布局添加view对象
 
         tvTitle.setText("新闻");
+        
+        //从服务器获取数据
+        getDataFromServer();
+    }
+
+    private void getDataFromServer() {
+        HttpUtils httpUtils = new HttpUtils();
+
+        httpUtils.send(HttpRequest.HttpMethod.GET, GlobalConstants.CATEGORY_URL, new RequestCallBack<String>() {
+
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String result = responseInfo.result;
+                System.out.println("服务器数据："+result);
+            }
+
+            @Override
+            public void onFailure(HttpException e, String msg) {
+                e.printStackTrace();
+                Toast.makeText(mActivity, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/yrj/zhbj/global/GlobalConstants.java
@@ -0,0 +1,11 @@
+package com.yrj.zhbj.global;
+
+/**
+ * 全局常量类
+ */
+public class GlobalConstants {
+
+    //原生模拟器：10.0.2.2，Genymotion模拟器：10.0.3.2
+    public static final String SERVER_URL = "http://10.10.10.122:8080/zhbj";
+    public static final String CATEGORY_URL = SERVER_URL + "/categories.json";
+}
